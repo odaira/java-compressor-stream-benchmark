416beba9b578d7e422d0de14b0fad666f1dd138e
Rei Odaira <rodaira@us.ibm.com>
2020-01-30 01:31:32 -0600

    Add snappy-java and zstd-jni

---
+++ src/main/java/net/odaira/jcsb/SnappyDriver.java
@@ -0,0 +1,35 @@
+package net.odaira.jcsb;
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import org.xerial.snappy.SnappyOutputStream;
+import org.xerial.snappy.SnappyInputStream;
+
+public class SnappyDriver extends Driver {
+
+    @Override
+    public OutputStream allocateOutputStream(final OutputStream out) throws IOException {
+	return new SnappyOutputStream(out, 64 * 1024);
+    }
+
+    @Override
+    public InputStream allocateInputStream(final InputStream in) throws IOException {
+	return new SnappyInputStream(in);
+    }
+}
